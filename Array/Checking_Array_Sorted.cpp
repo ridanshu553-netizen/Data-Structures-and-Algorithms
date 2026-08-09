f92c14d3ac2300c6e1d390f5f474e9b8bddd6874
Ridanshu Choudhary <ridanshu553@gmail.com>
2026-08-09 21:28:51 +0530

    Add array structure with sorting check and display
    Implement a structure to check if an array is sorted and display its elements.

---
+++ Array/Checking_Array_Sorted.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+struct Array{
+    int A[10];
+    int size;
+    int length;
+};
+void display(struct Array arr){
+    for(int i=0;i<arr.length;i++){
+        cout<<arr.A[i]<<endl;
+    }
+}
+int isSorted(struct Array arr){
+    for(int i=0;i<arr.length-1;i++){
+        if(arr.A[i]>arr.A[i+1]){
+            return 0;
+            break;
+        }
+    }
+    return 1;
+}
+int main(){
+    struct Array arr1={{1,2,3,4,5,6,7},7,7};
+    cout<<isSorted(arr1)<<endl;
+    display(arr1);
+}
