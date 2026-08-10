fde4379cda324f78435cfa8623233798c71cc31b
Ridanshu Choudhary <ridanshu553@gmail.com>
2026-08-10 11:46:24 +0530

    Add Library_Function.cpp with basic math functions

---
+++ Basics/Libraray_Function.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+
+    cout<<sqrt(9)<<endl;
+
+    cout<<min(4,9)<<endl;
+
+    cout<<max(4,9)<<endl;
+
+    cout<<pow(2,6)<<endl;
+    
+}
