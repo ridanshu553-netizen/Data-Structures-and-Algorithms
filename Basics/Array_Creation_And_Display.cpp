32a5bc5d4d4da7ca952eb7bbcc28730fdba3e8df
ridanshu553-netizen <ridanshu553@gmail.com>
2026-08-19 22:27:04 +0530

    Add array creation and display functionality in C++

---
+++ Basics/Array_Creation_And_Display.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[100], n;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+
+    cout << "Array: ";
+    for(int i = 0; i < n; i++)
+        cout << a[i] << " ";
+
+    return 0;
+}
