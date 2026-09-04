857f7488adb89ba033dc37bcbdfc47f17957c2c8
ridanshu553-netizen <ridanshu553@gmail.com>
2026-09-04 14:14:13 +0530

    Refactor spacing and formatting in Update_An_Element.cpp for improved readability

--- Basics/Update_An_Element.cpp
+++ Basics/Update_An_Element.cpp
@@ -1,19 +1,14 @@
 #include <iostream>
 using namespace std;
-
 int main() {
     int a[100], n, pos, x;
-
     cout << "Enter number of elements: ";
     cin >> n;
-
     cout << "Enter elements: ";
     for(int i = 0; i < n; i++)
         cin >> a[i];
-
     cout << "Enter index to update: ";
     cin >> pos;
-
     if(pos < 0 || pos >= n) {
         cout << "Invalid index";
         return 0;
